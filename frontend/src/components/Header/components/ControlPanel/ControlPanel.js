142b20aa50fa8d8c8b625c16efe43f11bf2f1f66
Никита Уманский <nikum_st@MACBOOK-PRO.station>
2025-05-19 19:40:14 +0200

    changed url for userBookingImage

--- frontend/src/components/Header/components/ControlPanel/ControlPanel.js
+++ frontend/src/components/Header/components/ControlPanel/ControlPanel.js
@@ -3,6 +3,7 @@ import styles from './ControlPanel.module.css';
 import { useSelector } from 'react-redux';
 import { selectIsAuthenticated } from '../../../../store';
 import { Authenticated } from './components/Authenticated';
+import { Icon } from '../../../components/Icon/Icon';
 
 export const ControlPanel = () => {
 	const isAuthenticated = useSelector(selectIsAuthenticated);
@@ -11,6 +12,9 @@ export const ControlPanel = () => {
 		<Authenticated />
 	) : (
 		<div className={styles.controlPanel}>
+			<Link to="/rooms">
+				<Icon size={'30px'} id={'fa fa-bed'} title="All rooms" />
+			</Link>
 			<Link to="/authorize">
 				<button>Log In</button>
 			</Link>
